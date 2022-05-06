29cbac847cad61d6812b3c1f0773635e2f472821
Jessica Bender <jessicabednder18@gmail.com>
2022-05-06 17:02:57 -0500

    start of form for add

--- src/components/Forms/AddCar/AddCar.jsx
+++ src/components/Forms/AddCar/AddCar.jsx
@@ -1,11 +1,34 @@
-import { Typography } from "@mui/material";
+import { Button, FormControl, TextField } from "@mui/material";
 import React from "react";
 
 class AddCars extends React.Component{
     render(){
         return(
-            <div>
-                <Typography variant="h5">Add Cars Form </Typography>
+            <div className="Add">
+                <FormControl>
+                    <div className="form">
+                        <TextField id="outlined-basic" label="Car Make" variant="outlined" />
+                        <TextField id="outlined-basic" label="Car Model" variant="outlined" />
+                    </div>
+                    <div className="form">
+                        <TextField id="outlined-basic" label="Car Color" variant="outlined" />
+                        <TextField id="outlined-basic" label="Car Price per Day" variant="outlined" />
+                    </div>
+                    <div className="form">
+                        <TextField id="outlined-basic" label="Car Year" variant="outlined" />
+                        <TextField id="outlined-basic" label="Car Lience Plate Number" variant="outlined" />
+                    </div>
+                    <div className="form">
+                        <TextField id="outlined-basic" label="Car Desciption" variant="outlined" />
+                    </div>
+                    <div className="form">
+                        <TextField id="outlined-basic" label="Lot Number" variant="outlined" />
+                        <TextField id="outlined-basic" label="Parking Spot Number" variant="outlined" />
+                    </div>
+                    <Button className="SelcectCar">
+                        Add Car
+                    </Button>
+                </FormControl>
             </div>
         )
     }
